61f7eb3eef5d372eb4c65383959203f58874cf35
adrian <adrian@google.com>
2023-12-04 10:13:15 -0400

    Agregando el modelado de HANGARES y PILOTOS

---
+++ ProyectoBackendAdrian/ProyectoBackendAdrian/Models/Hangar.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoBackendAdrian.Models
+{
+    public class Hangar
+    {
+        public int numero_hangar {  get; set; }
+        public string capacidad { get; set; }
+        public string localizacion { get; set; }
+    }
+}
---
+++ ProyectoBackendAdrian/ProyectoBackendAdrian/Models/Piloto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoBackendAdrian.Models
+{
+    public class Piloto
+    {
+        public int numero_licencia {  get; set; }
+        public string? restricciones { get; set; }
+
+        public string? salario 
+        public string? turno
+    }
+}
