fb4f47540a9d76ca3ebccbc65568264c61b12565
adrian <adrian@google.com>
2023-12-04 10:09:43 -0400

    Agregando el modelado de AVIONES

---
+++ ProyectoBackendAdrian/ProyectoBackendAdrian/Models/Aviones.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoBackendAdrian.Models
+{
+    public class Aviones
+    {
+        public int numero {  get; set; }
+        public string modelo { get; set; }
+        public string peso { get; set; }
+        public string capacidad {  get; set; }
+    }
+}
